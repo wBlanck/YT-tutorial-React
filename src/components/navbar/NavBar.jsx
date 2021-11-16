892e87fbe30b48a1227ca44eecc2052f92d70adc
wblanck - laptop <blanck.william@gmail.com>
2021-11-16 10:20:58 +0100

    added all the assets

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from  'react-icons/ri';
 import './navbar.css';
 
 const NavBar = () => {
   return (
-    <div>
-      NavBar
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links__logo">
+          <img src={logo} alt="Logo" />
+        </div>
+      </div>
     </div>
   )
 }
